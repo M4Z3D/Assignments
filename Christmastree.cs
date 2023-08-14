a76aaf7d20af7bfc930fdadd9912139124f90931
M4Z3D <118048523+M4Z3D@users.noreply.github.com>
2023-08-14 21:49:22 +0200

    Add files via upload

---
+++ Christmastree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // you can change the height of the tree by changing the height number
+        int height = 8;
+        int spaces = height - 1;
+        bool isGreen = false;
+        // This for loop runs through the height of the tree
+        for (int i = 0; i < height; i++)
+        {
+            // This loop will write out the leading spaces required for the tree to be resembled
+            for (int j = 0; j < spaces; j++)
+            {
+                Console.Write(" ");
+            }
+            // this will decide if the stars are either green or red
+            for (int j = 0; j < 2 * i + 1; j++)
+            {
+                if (isGreen)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("*");
+                    Console.ResetColor();
+                }
+                else 
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("*");
+                    Console.ResetColor();
+                }
+
+                isGreen = !isGreen;
+            }
+
+            // decreasing the spaces by 1 for each run
+            Console.WriteLine();
+            spaces--;
+        }
+    }
+}
---
+++ Nim.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Principal;
+
+namespace Nim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalSticks = 7;
+            bool isHumanTurn = true;
+            int pickedSticks = 0;
+
+            Console.WriteLine("Lets play Nim!");
+
+            while (totalSticks > 0)
+            {
+                Console.WriteLine("There are " + totalSticks + " Sticks left on the table");
+
+                int sticksToTake = 0;
+
+                if (isHumanTurn)
+                {
+                    do
+                    {
+                        Console.Write("Pick between how many sticks you want to remove (1-3)");
+                        int sticksTotake int.Parse(Console.ReadLine());
+
+                    } while (!int.TryParse(Console.ReadLine())); sticksToTake >= totalSticks;
+                }
+            }
+
+        }
+    }
+}
